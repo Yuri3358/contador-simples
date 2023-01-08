d4f021c2312a0275bf6609ccffd4fd1942c58a5b
Yuri3358 <yurialmeida917@gmail.com>
2023-01-08 10:56:48 -0300

    feat: dynamic color change

--- components/counterComponent.js
+++ components/counterComponent.js
@@ -1,11 +1,26 @@
 const counterComponent = {
     template: 
     `<p id="counter">{{ count }}</p>
-    <button class="btn btn-primary" @click="count++">Acrescentar</button>
-    <button class="btn btn-secondary" @click="count--">Diminuir</button>`,
+    <button class="btn btn-primary" @click="increment">Acrescentar</button>
+    <button class="btn btn-secondary" @click="increment">Diminuir</button>`,
     data() {
         return {
             count: 0
+        }
+    },
+    methods: {
+        increment(){
+            if (this.count >= 0) {
+                document.getElementById("counter").style.color = "black"
+            }
+            this.count++
+        },
+
+        decrement(){
+            if (this.count <= 0) {
+                document.getElementById("counter").style.color = "red"
+            }
+            this.count--
         }
     }
 }
